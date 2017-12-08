94a52158ca5a78cc47327f6cd26626a78e1c655d
Emmanuel Audu <Scargger2010560@gmail.com>
2017-12-08 16:41:54 +0100

    8-12-2017

--- public_html/fxacademy/layouts/header.php
+++ public_html/fxacademy/layouts/header.php
@@ -2,7 +2,7 @@
     <div class="container-fluid no-gutter masthead">
         <div class="row">
             <div id="main-logo" class="col-sm-12 col-md-5">
-                    <a href="fxacademy/index.php"><img src="images/ifxlogo.png" alt="Instaforex Nigeria Logo" /></a>
+                    <a href="fxacademy/index.php"><img src="images/ifxlogo-xmas.png" alt="Instaforex Nigeria Logo" /></a>
             </div>
             <div id="top-nav" class="col-sm-12 col-md-7 text-right">
             </div>
--- public_html/layouts/header.php
+++ public_html/layouts/header.php
@@ -3,7 +3,7 @@
             <div class="container-fluid no-gutter masthead">
                 <div class="row">
                     <div id="main-logo" class="col-sm-12 col-md-5">
-                        <a href="./" title="Home Page"><img src="images/ifxlogo.png" alt="Instaforex Nigeria Logo" /></a>
+                        <a href="./" title="Home Page"><img src="images/ifxlogo-xmas.png" alt="Instaforex Nigeria Logo" /></a>
                     </div>
                     <div id="top-nav" class="col-sm-12 col-md-7 text-right">
                         <blockquote id="header-blockquote" class="blockquote-reverse">
