82b268b855d527ffd0530c577f8c00e900e27e36
Emmanuel Audu <Scargger2010560@gmail.com>
2018-03-23 08:44:14 +0100

    Text corrections to loyalty page

--- public_html/admin/prospect_ilpr_manage.php
+++ public_html/admin/prospect_ilpr_manage.php
@@ -161,8 +161,7 @@ if(isset($_POST['live_account_ilpr_reg'])) {
                                     </thead>
                                     <tbody>
                                     <?php if(isset($all_prospect) && !empty($all_prospect)) {
-                                        foreach ($all_prospect as $row) {
-                                            $fullname = $row['f_name']." ".$row['m_name']." ".strtoupper($row['l_name']) ?>
+                                        foreach ($all_prospect as $row) {?>
                                             <tr>
                                                 <td><?php echo $row['f_name']." ".$row['m_name']." ".strtoupper($row['l_name']); ?></td>
                                                 <td><?php echo $row['email']; ?></td>
@@ -206,57 +205,40 @@ if(isset($_POST['live_account_ilpr_reg'])) {
                                                                             </form>
                                                                         </div>
                                                                         <div id="successfull" class="tab-pane fade">
-                                                                            <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="">
-                                                                            <p>You have <strong>successfully</strong> contacted <?php echo $fullname; ?>.</p>
-                                                                                <h3>Open ILPR Account</h3>
-
-<!--                                                                                open live account-->
-                                                                                <ul class="fa-ul">
-                                                                                    <li><i class="fa-li fa fa-check-square-o icon-tune"></i>Step 2: Enrol Your Account Into INSTAFXNG LOYALTY PROGRAM AND REWARDS (ILPR) for FREE</li>
-                                                                                </ul>
-
-                                                                                <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
-                                                                                    <div class="form-group">
-                                                                                        <label class="control-label col-sm-3" for="full_name">Full Name:</label>
-                                                                                        <div class="col-sm-9 col-lg-5">
-                                                                                            <input name="full_name" type="text" class="form-control" id="full_name" value="<?php echo $fullname; ?>" required>
-                                                                                        </div>
+                                                                            <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+                                                                                <p>You have <strong>successfully</strong> contacted <?php echo $row['f_name']." ".$row['m_name']." ".$row['l_name']; ?>.
+                                                                                    <br/>Enrol his/her Instaforex Account into the INSTAFXNG LOYALTY PROGRAM AND REWARDS (ILPR).
+                                                                                </p>
+                                                                                <div class="form-group">
+                                                                                    <label class="control-label col-sm-3" for="full_name">Full Name:</label>
+                                                                                    <div class="col-sm-9 col-lg-5">
+                                                                                        <input name="full_name" type="text" class="form-control" id="full_name" value="<?php echo $row['f_name']." ".$row['m_name']." ".$row['l_name']; ?>" required>
                                                                                     </div>
-                                                                                    <div class="form-group">
-                                                                                        <label class="control-label col-sm-3" for="email">Email Address:</label>
-                                                                                        <div class="col-sm-9 col-lg-5">
-                                                                                            <input name="email" type="text" class="form-control" id="email" value="<?php echo $row['email']; ?>" required>
-                                                                                        </div>
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <label class="control-label col-sm-3" for="email">Email Address:</label>
+                                                                                    <div class="col-sm-9 col-lg-5">
+                                                                                        <input name="email" type="text" class="form-control" id="email" value="<?php echo $row['email']; ?>" required>
                                                                                     </div>
-                                                                                    <div class="form-group">
-                                                                                        <label class="control-label col-sm-3" for="phone">Phone Number:</label>
-                                                                                        <div class="col-sm-9 col-lg-5">
-                                                                                            <input name="phone" type="text" class="form-control" id="phone" value="<?php echo $row['phone']; ?>" required>
-                                                                                        </div>
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <label class="control-label col-sm-3" for="phone">Phone Number:</label>
+                                                                                    <div class="col-sm-9 col-lg-5">
+                                                                                        <input name="phone" type="text" class="form-control" id="phone" value="<?php echo $row['phone']; ?>" required>
                                                                                     </div>
-                                                                                    <div class="form-group">
-                                                                                        <label class="control-label col-sm-3" for="ifx_acct_no">Instaforex Account Number:</label>
-                                                                                        <div class="col-sm-9 col-lg-5">
-                                                                                            <input name="ifx_acct_no" type="text" class="form-control" id="ifx_acct_no" required>
-                                                                                        </div>
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <label class="control-label col-sm-3" for="ifx_acct_no">Instaforex Account Number:</label>
+                                                                                    <div class="col-sm-9 col-lg-5">
+                                                                                        <input name="ifx_acct_no" type="text" class="form-control" id="ifx_acct_no" required>
                                                                                     </div>
-                                                                                    <div class="form-group">
-                                                                                        <label class="control-label col-sm-3" for="recaptcha">&nbsp;</label>
-                                                                                        <div class="col-sm-9 col-lg-5 g-recaptcha" data-sitekey="6LcKDhATAAAAAF3bt-hC_fWA2F0YKKpNCPFoz2Jm"></div>
-                                                                                    </div>
-                                                                                    <div class="form-group">
-                                                                                        <div class="col-sm-offset-3 col-sm-9"><input name="live_account_ilpr_reg" type="submit" class="btn btn-success" value="Submit" /></div>
-                                                                                    </div>
-                                                                                </form>
-
-
-
+                                                                                </div>
+                                                                                <div class="form-group">
+                                                                                    <div class="col-sm-offset-3 col-sm-9"><input name="live_account_ilpr_reg" type="submit" class="btn btn-success" value="Submit" /></div>
+                                                                                </div>
                                                                             </form>
                                                                         </div>
                                                                     </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="submit" name="close" onClick="window.close();" data-dismiss="modal" class="btn btn-danger">Close!</button>
-                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
