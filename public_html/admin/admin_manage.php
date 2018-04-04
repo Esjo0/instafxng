7249e8b598a26b78b2017c3916a89c663f86a1de
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-04-04 12:26:27 +0100

    admin previledge

--- public_html/admin/admin_manage.php
+++ public_html/admin/admin_manage.php
@@ -310,7 +310,7 @@ $my_pages = explode(",", $my_pages['allowed_pages']);
                                         <div class="col-sm-4"><div class="checkbox"><label for=""><input type="checkbox" name="pageid[]" value="238" id="" <?php if (in_array(238, $my_pages)) { echo 'checked="checked"'; } ?>/> Category 4 Students</label></div></div>
                                         <div class="col-sm-4"><div class="checkbox"><label for=""><input type="checkbox" name="pageid[]" value="73" id="" <?php if (in_array(73, $my_pages)) { echo 'checked="checked"'; } ?>/> Training Campaign</label></div></div>
                                         <div class="col-sm-4"><div class="checkbox"><label for=""><input type="checkbox" name="pageid[]" value="74" id="" <?php if (in_array(74, $my_pages)) { echo 'checked="checked"'; } ?>/> Forum Registration</label></div></div>
-                                        <div class="col-sm-4"><div class="checkbox"><label for=""><input type="checkbox" name="pageid[]" value="74" id="" <?php if (in_array(247, $my_pages)) { echo 'checked="checked"'; } ?>/> Schedule Traders Forum</label></div></div>
+                                        <div class="col-sm-4"><div class="checkbox"><label for=""><input type="checkbox" name="pageid[]" value="247" id="" <?php if (in_array(247, $my_pages)) { echo 'checked="checked"'; } ?>/> Schedule Traders Forum</label></div></div>
                                     </div>
                                     <hr/>
 
