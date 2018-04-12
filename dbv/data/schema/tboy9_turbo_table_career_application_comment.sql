da26dd2a85a0ce08080040c103642657d1f28091
Emmanuel Audu <Scargger2010560@gmail.com>
2018-04-12 14:27:20 +0100

    Article Mail Notifications

--- dbv/data/schema/tboy9_turbo_table_career_application_comment.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE `career_application_comment` (
-  `career_application_comment_id` int(11) NOT NULL,
-  `application_id` int(11) NOT NULL,
-  `admin_code` varchar(5) NOT NULL,
-  `comment` text NOT NULL,
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- dbv/data/schema/tboy9_turbo_table_signal_symbol.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE TABLE `signal_symbol` (
-  `signal_symbol_id` int(11) NOT NULL,
-  `symbol` varchar(10) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
