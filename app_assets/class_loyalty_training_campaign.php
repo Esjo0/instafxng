65c943eaa85b952f17770ebbc1c203d23b15b548
Emmanuel Audu <Scargger2010560@gmail.com>
2018-04-18 11:56:41 +0100

    no message

--- app_assets/class_loyalty_training_campaign.php
+++ app_assets/class_loyalty_training_campaign.php
@@ -128,20 +128,21 @@ MAIL;
             <p>I would like to take this opportunity to let you know how pleased and excited I am that you have chosen to trade with InstaForex Nigeria (www.InstaFxNg.com).</p>
             <p>You have joined over 14,000 Nigerians who make consistent income from the Forex market using the InstaForex platform and also earn more money just for trading.</p>
             <p>At InstaFxNg, we consider it a privilege to serve and provide you with excellent and unparalleled service at all times.</p>
-           
-            <p>We have been around for over 7 years providing Forex services to thousands of Nigerian traders, 
-            ensuring that their deposit and withdrawal transactions are promptly responded to and that every 
-            challenge is totally resolved.</p>
-            <p>InstaForex Nigeria is built upon seven strong foundational values; Integrity, Commitment, Speed, 
-            Focus, Empathy, Reliability, and Innovation.</p>
-            <p>From making deposit into your account (locally and easily) to instant stress free withdrawals (to your bank account) to unmatched customer support, we have you covered. </p>
-            <p>We are dedicated to working effectively to ensure swift service delivery to you consistently.</p>
-            <p>To start your journey to earning more money in our Loyalty Rewards Program, kindly click <a href="https://instafxng.com/live_account.php?id=lp">here</a> to open an InstaForex account immediately.</p>
-            <p>Very shortly you're going to receive a call welcoming you on board from our ever 
-            cheerful customer service representatives who will assist you in opening an InstaForex 
-            account so you can get started immediately.</p>
-            <p>Don't forget to click <a href="https://instafxng.com/live_account.php?id=lp">here</a> to open your InstaForex account. You can also click <a href="https://instafxng.com/deposit_funds.php">here</a> to fund your account so you can get started immediately.</p>
-            <p>I'm so glad you are here and I can't wait for you to start earning from your trades and in the InstaFxNg Loyalty Points and Reward Program.</p>
+            <p>We have been around for over 7 years providing excellent forex services to thousands of Nigerian traders, we are reliable and your funds are safe in our hands.</p>
+            <p><strong>Hey! What’s Special? Why InstaFxNg? Here’s why….</strong></p>
+            <p>At InstaFxNg here are some benefits you won’t find elsewhere;</p>
+            <ul>
+                <li><b>Instant funding of account: </b>No holding back! No Long Procedures! You can make instant withdrawals directly into your local bank account. We will process all transactions you initiate on our platform immediately. </li>
+                <li><b>130% Welcome Bonus: </b>Do you know that for your first Deposit of either $50, $100 or $150 you will get a whopping 130% bonus? Oh yes you can! So what are you waiting for? </li>
+                <li><b>InstaFxNg Loyalty Program: </b>Yes! You will get rewarded for trading! We constantly plan rewards for our consistent traders to ensure that you have an exciting experience! So while you are trading you will earn points which can be redeemed, 5 leading traders will be rewarded monthly and N2,250,000 is up for grabs yearly! You can be the next lucky winner if you start now! </li>
+                <li><b>Excellent Customer Service: </b>In addition, there is an extra topping on the cake! Our customer service executives will handle all other matters relating to your account while you trade. We will also follow through to ensure that all inquiries or queries are resolved promptly. </li>
+            </ul>
+            <p><b>Are you ready to begin your journey with us?</b></p>
+            <p>To start your journey to earning more money in our Loyalty Rewards Program, 
+            kindly <a href="https://instafxng.com/loyalty/live_account.php">click here to open an InstaForex 
+            account</a> immediately if you haven’t done that already.</p>
+            <p><center><b>Open My InstaForex Account</b></center></p>
+            <p>I’m so glad you are here and I can’t wait for you to start earning in the <a href="https://instafxng.com/loyalty.php">InstaFxNg Loyalty Points and Reward Program</a></p>
             <br/><br/>
             <p>Best Regards,</p>
             <p>Mercy,</p>
