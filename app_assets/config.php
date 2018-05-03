4cede3e554bd478cd347255603b782a6dc067904
Emmanuel Audu <Scargger2010560@gmail.com>
2018-05-03 15:53:08 +0100

    Updates

--- app_assets/config.php
+++ app_assets/config.php
@@ -4,7 +4,7 @@
  defined('DB_USER')   ? null : define("DB_USER", "root");
  defined('DB_PASS')   ? null : define("DB_PASS", "");
 
- defined('DB_NAME')   ? null : define("DB_NAME", "tboy9_turbo");
+ defined('DB_NAME')   ? null : define("DB_NAME", "instafxng");
 
 //defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
 //defined('DB_USER')   ? null : define("DB_USER", "root");
