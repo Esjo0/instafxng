50e6396933e654b4c0bb5353085be743e97d08c6
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-05-16 09:41:31 +0100

    Muted text below all recaptcha buttons
    Help! Us fight Spam

--- public_html/loyalty/index.php
+++ public_html/loyalty/index.php
@@ -197,11 +197,11 @@ if(isset($_POST['sign_up']))
 										</div>
 									</div>
 								</div>
-                                <center style="color: #0d95e8"><strong> Help <i class="fa fa-exclamation"></i></strong> Us Fight Spam.</center>
 								<div class="form-group">
 									<center><div class="g-recaptcha" data-sitekey="6LcKDhATAAAAAF3bt-hC_fWA2F0YKKpNCPFoz2Jm"></div></center>
 								</div>
-								<div class="col-sm-12">
+                                <center class="text-muted"><strong> Help <i class="fa fa-exclamation"></i></strong> Us Fight Spam.</center>
+                                <div class="col-sm-12">
 									<span style="color: #ffffff">*All fields are required</span>
 								</div>
 								<div class="form-group">
