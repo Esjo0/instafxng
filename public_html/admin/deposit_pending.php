b3e0af951788325e39c70d41f66b28b05ac3e5fa
Emmanuel Audu <Scargger2010560@gmail.com>
2018-06-01 11:17:39 +0100

    Updates

--- public_html/admin/deposit_pending.php
+++ public_html/admin/deposit_pending.php
@@ -6,14 +6,24 @@ if (!$session_admin->is_logged_in()) {
 
 $client_operation = new clientOperation();
 
-$query = "SELECT ud.trans_id, ud.dollar_ordered, ud.created, ud.naira_total_payable,
+/*$query = "SELECT ud.trans_id, ud.dollar_ordered, ud.created, ud.naira_total_payable,
         ui.ifx_acct_no, CONCAT(u.last_name, SPACE(1), u.first_name) AS full_name, u.phone,
         uc.passport, ui.ifxaccount_id
         FROM user_deposit AS ud
         INNER JOIN user_ifxaccount AS ui ON ud.ifxaccount_id = ui.ifxaccount_id
         INNER JOIN user AS u ON ui.user_code = u.user_code
         LEFT JOIN user_credential AS uc ON ui.user_code = uc.user_code
-        WHERE ud.status = '1' ORDER BY ud.user_deposit_id DESC ";
+        WHERE ud.status = '1' ORDER BY ud.user_deposit_id DESC ";*/
+$query = "SELECT ud.trans_id, ud.dollar_ordered, ud.created, ud.naira_total_payable,
+        ui.ifx_acct_no, CONCAT(u.last_name, SPACE(1), u.first_name) AS full_name, u.phone,
+        uc.passport, ui.ifxaccount_id, MAX(ud.user_deposit_id) AS user_deposit_id , u.user_code 
+        FROM user_deposit AS ud
+        INNER JOIN user_ifxaccount AS ui ON ud.ifxaccount_id = ui.ifxaccount_id
+        INNER JOIN user AS u ON ui.user_code = u.user_code
+        LEFT JOIN user_credential AS uc ON ui.user_code = uc.user_code
+        WHERE ud.status = '1' 
+        GROUP BY u.user_code 
+        ORDER BY ud.user_deposit_id DESC ";
 $numrows = $db_handle->numRows($query);
 
 $rowsperpage = 20;
@@ -37,6 +47,34 @@ $query .= 'LIMIT ' . $offset . ',' . $rowsperpage;
 $result = $db_handle->runQuery($query);
 $pending_deposit_requests = $db_handle->fetchAssoc($result);
 
+function get_pending_transactions($user_code)
+{
+    global $db_handle;
+    $query = "SELECT ud.trans_id, ud.dollar_ordered, ud.created, ud.naira_total_payable, ui.ifx_acct_no, ui.ifxaccount_id, ud.user_deposit_id, u.user_code 
+FROM user_deposit AS ud
+INNER JOIN user_ifxaccount AS ui ON ud.ifxaccount_id = ui.ifxaccount_id
+INNER JOIN user AS u ON ui.user_code = u.user_code
+WHERE ud.status = '1' 
+AND u.user_code = '$user_code' 
+ORDER BY ud.user_deposit_id DESC ";
+    $result = $db_handle->fetchAssoc($db_handle->runQuery($query));
+    return $result;
+}
+
+function bulk_sms_url($from, $to)
+{
+    global $db_handle;
+    $query = "SELECT ud.trans_id, ud.dollar_ordered, ud.created, ud.naira_total_payable,
+        ui.ifx_acct_no, CONCAT(u.last_name, SPACE(1), u.first_name) AS full_name, u.phone,
+        uc.passport, ui.ifxaccount_id, MAX(ud.user_deposit_id) AS user_deposit_id , u.user_code 
+        FROM user_deposit AS ud
+        INNER JOIN user_ifxaccount AS ui ON ud.ifxaccount_id = ui.ifxaccount_id
+        INNER JOIN user AS u ON ui.user_code = u.user_code
+        LEFT JOIN user_credential AS uc ON ui.user_code = uc.user_code
+        WHERE ud.status = '1' 
+        GROUP BY u.user_code 
+        ORDER BY ud.user_deposit_id DESC ";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -60,70 +98,74 @@ $pending_deposit_requests = $db_handle->fetchAssoc($result);
         <div id="main-body-side-bar" class="col-md-4 col-lg-3 left-nav">
             <?php require_once 'layouts/sidebar.php'; ?>
         </div>
-
         <!-- Main Body - Content Area: This is the main content area, unique for each page  -->
         <div id="main-body-content-area" class="col-md-8 col-lg-9">
-
             <!-- Unique Page Content Starts Here
             ================================================== -->
-
             <div class="row">
                 <div class="col-sm-12 text-danger">
                     <h4><strong>PENDING DEPOSIT</strong></h4>
                 </div>
             </div>
-
             <div class="section-tint super-shadow">
                 <div class="row">
                     <div class="col-sm-12">
                         <?php require_once 'layouts/feedback_message.php'; ?>
-
+                        <p class="pull-right"><a class="btn btn-sm btn-default">Send Bulk SMS For <?php echo  date_to_text(date('Y-m-d'));?>  <i class="glyphicon glyphicon-arrow-right"></i></a>
                         <p>Deposit Transactions Initiated by Clients. These transactions have not been notified.</p>
 
-                        <table class="table table-responsive table-striped table-bordered table-hover">
+                            <?php if(isset($pending_deposit_requests) && !empty($pending_deposit_requests)) {foreach ($pending_deposit_requests as $row) {?>
+                        <table class="table table-responsive table-striped table-hover">
                             <thead>
-                            <tr>
-                                <th>Transaction ID</th>
-                                <th>Client Name</th>
-                                <th>IFX Account</th>
-                                <th>Phone Number</th>
-                                <th>Amount Ordered</th>
-                                <th>Total Payable</th>
-                                <th>Date Created</th>
-                                <th>Action</th>
-                            </tr>
+                                <tr>
+                                    <th>Client Details</th>
+                                    <th>Transaction ID</th>
+                                    <th>IFX Account</th>
+                                    <th>Amount Ordered</th>
+                                    <th>Total Payable</th>
+                                    <th>Date Created</th>
+                                    <th>Action</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            <?php
-                            if(isset($pending_deposit_requests) && !empty($pending_deposit_requests)) {
-                                foreach ($pending_deposit_requests as $row) {
-                                    ?>
+                                <?php $transactions = get_pending_transactions($row['user_code']);?>
                                     <tr>
-                                        <td><?php echo $row['trans_id']; ?></td>
-                                        <td><?php echo $row['full_name']; ?></td>
-                                        <td><?php echo $row['ifx_acct_no']; ?></td>
-                                        <td><?php echo $row['phone']; ?></td>
-                                        <td class="nowrap">&dollar; <?php echo number_format($row['dollar_ordered'], 2, ".", ","); ?></td>
-                                        <td class="nowrap">&#8358; <?php echo number_format($row['naira_total_payable'], 2, ".", ","); ?></td>
-                                        <td><?php echo datetime_to_text($row['created']); ?></td>
+                                        <td rowspan="<?php echo count(get_pending_transactions($row['user_code'])); ?>"><?php echo $row['full_name']; ?><br/><?php echo $row['phone']; ?></td>
+                                        <td><?php echo $transactions[0]['trans_id']; ?></td>
+                                        <td><?php echo $transactions[0]['ifx_acct_no']; ?></td>
+                                        <td class="nowrap">&dollar; <?php echo number_format($transactions[0]['dollar_ordered'], 2, ".", ","); ?></td>
+                                        <td class="nowrap">&#8358; <?php echo number_format($transactions[0]['naira_total_payable'], 2, ".", ","); ?></td>
+                                        <td><?php echo datetime_to_text($transactions[0]['created']); ?></td>
                                         <td class="nowrap">
-                                            <a class="btn btn-info" href="deposit_pay_notify.php?id=<?php echo encrypt($row['trans_id']); ?>" title="Payment Notification"><i class="fa fa-bell-o" aria-hidden="true"></i></a>
-                                            <a class="btn btn-info" href="deposit_process.php?x=pending&id=<?php echo encrypt($row['trans_id']); ?>" title="Comment"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
+                                            <a class="btn btn-xs btn-info" href="deposit_pay_notify.php?id=<?php echo encrypt($transactions[0]['trans_id']); ?>" title="Payment Notification"><i class="fa fa-bell-o" aria-hidden="true"></i></a>
+                                            <a class="btn btn-xs btn-info" href="deposit_process.php?x=pending&id=<?php echo encrypt($transactions[0]['trans_id']); ?>" title="Comment"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
                                         </td>
                                     </tr>
-                                <?php } } else { echo "<tr><td colspan='7' class='text-danger'><em>No results to display</em></td></tr>"; } ?>
+                                <?php unset($transactions[0]);?>
+                                <?php foreach ($transactions as $key){ ?>
+                                    <tr>
+                                        <td><?php echo $key['trans_id']; ?></td>
+                                        <td><?php echo $key['ifx_acct_no']; ?></td>
+                                        <td class="nowrap">&dollar; <?php echo number_format($key['dollar_ordered'], 2, ".", ","); ?></td>
+                                        <td class="nowrap">&#8358; <?php echo number_format($key['naira_total_payable'], 2, ".", ","); ?></td>
+                                        <td><?php echo datetime_to_text($key['created']); ?></td>
+                                        <td class="nowrap">
+                                            <a class="btn btn-xs btn-info" href="deposit_pay_notify.php?id=<?php echo encrypt($key['trans_id']); ?>" title="Payment Notification"><i class="fa fa-bell-o" aria-hidden="true"></i></a>
+                                            <a class="btn btn-xs btn-info" href="deposit_process.php?x=pending&id=<?php echo encrypt($key['trans_id']); ?>" title="Comment"><i class="fa fa-comments-o" aria-hidden="true"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
                             </tbody>
-                        </table>
+                        </table><br/><br/>
+                                <?php } } else { echo "<table><thead><th>Client Details</th><th>Transaction ID</th><th>IFX Account</th><th>Amount Ordered</th><th>Total Payable</th><th>Date Created</th><th>Action</th></thead><tbody><tr><td colspan='7' class='text-danger'><em>No results to display</em></td></tr></tbody></table>"; } ?>
 
                         <?php if(isset($pending_deposit_requests) && !empty($pending_deposit_requests)) { ?>
                             <div class="tool-footer text-right">
                                 <p class="pull-left">Showing <?php echo $prespagelow . " to " . $prespagehigh . " of " . $numrows; ?> entries</p>
                             </div>
                         <?php } ?>
-
                     </div>
                 </div>
-
                 <?php if(isset($pending_deposit_requests) && !empty($pending_deposit_requests)) { require_once 'layouts/pagination_links.php'; } ?>
             </div>
 
