027538dcca7ebae1bc19618218a4e4c5d2ddf125
SLUDASOFT\Sludasoft <sludasoft@gmail.com>
2018-06-06 13:13:32 +0100

    modifications

--- app_assets/config_sys.php
+++ app_assets/config_sys.php
@@ -65,7 +65,8 @@ $client_group_DEFAULT = array(
     "48" => "Last Months New Clients without Accounts and have no Training",
     "49" => "Last Months New Clients without Accounts and have Training",
     "50" => "Last Months New Clients not yet funded above $50",
-    "51" => "April 2018 ILPR campaign leads"
+    "51" => "Clients that funded before March 22, 2018 and haven't funded till date.",
+    "52" => "April 2018 ILPR campaign leads"
 );
 $client_group_DESC = array(
     "1" => "*Consist of all clients that have at anytime come into our system.",
@@ -118,5 +119,6 @@ $client_group_DESC = array(
     "48" => "*All clients who came into the system last month who have NOT opened Instaforex accounts and have not gone through our training ",
     "49" => "*All clients who came into the system last month, have opened Instaforex accounts and have undergone online training",
     "50" => "*All New clients who havn't funded up to $50",
-    "51" => "*Campaign leads who came into our system in April 2018"
+    "51" => "Clients that funded before March 22, 2018 and haven't funded till date.",
+    "52" => "*Campaign leads who came into our system in April 2018"
     );
