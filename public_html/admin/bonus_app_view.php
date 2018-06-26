d784d2ae5e1c01f740c36a1eceba3b933d4ba69b
Emmanuel Audu <Scargger2010560@gmail.com>
2018-06-26 10:30:59 +0100

    Updates

--- public_html/admin/bonus_app_view.php
+++ public_html/admin/bonus_app_view.php
@@ -2,10 +2,32 @@
 require_once("../init/initialize_admin.php");
 if (!$session_admin->is_logged_in()) {redirect_to("login.php");}
 $bonus_operations = new Bonus_Operations();
+
+//TODO: Get a list all the reasons a bonus application can be declined
+if(isset($_POST['process-app'])){
+    $_app_id = $db_handle->sanitizePost($_POST['app_id']);
+    $_allocated_amount = $db_handle->sanitizePost($_POST['allocated_amount']);
+    $result = $bonus_operations->approve_app($_app_id, $_allocated_amount, $_SESSION['admin_unique_code']);
+    $result ? $message_success = "Operation Successful" : $message_error = "Operation Failed";
+}
+if(isset($_POST['process-dec'])){
+    $_app_id = $db_handle->sanitizePost($_POST['app_id']);
+    $_reasons = $db_handle->sanitizePost($_POST['reasons']);
+    $result = $bonus_operations->decline_app($_app_id, $_reasons, $_SESSION['admin_unique_code']);
+    $result ? $message_success = "Operation Successful" : $message_error = "Operation Failed";
+}
+
+
 $app_id = decrypt_ssl(str_replace(" ", "+", $_GET['app_id']));
 $app_details = $bonus_operations->get_app_by_id($app_id);
 if(empty($app_details)) {redirect_to("bonus_app_moderation.php");}
 $conditions = $bonus_operations->get_conditions_by_code($app_details['bonus_code']);
+
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,7 +65,74 @@ $conditions = $bonus_operations->get_conditions_by_code($app_details['bonus_code
                         <div class="row">
                             <div class="col-sm-12">
                                 <?php require_once 'layouts/feedback_message.php'; ?>
-                                <p class="pull-left"><a href="bonus_app_moderation.php" class="btn btn-default" title="Bonus Applications"><i class="fa fa-arrow-circle-left"></i> Bonus Applications</a></p>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                    <p class="pull-left"><a href="bonus_app_moderation.php" class="btn btn-default" title="Bonus Applications"><i class="fa fa-arrow-circle-left"></i> Bonus Applications</a></p>
+                                    <p class="pull-right">
+                                        <button data-toggle="modal" data-target="#app_decline" class="btn btn-sm btn-danger" title="Decline Application."><b style="font-size: medium">&times;</b> Decline</button>
+                                        <button data-toggle="modal" data-target="#app_approve" class="btn btn-sm btn-success" title="Approve Application"><b style="font-size: medium"><i class="fa fa-check"></i></b> Approve</button>
+                                    </p>
+                                    </div>
+                                </div>
+
+                                <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="">
+                                    <div id="app_decline" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" data-dismiss="modal" aria-hidden="true"  class="close">&times;</button>
+                                                    <h4 class="modal-title">Decline Bonus Application</h4></div>
+                                                <div class="modal-body">
+                                                    <input type="hidden" name="app_id" value="<?php echo $app_id; ?>" />
+                                                    <textarea maxlength="255" placeholder="Please state explicitly why this application is being denied." rows="4" class="form-control" name="reasons" required></textarea>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <input name="process-dec" type="submit" class="btn btn-sm btn-success" value="Proceed">
+                                                    <button type="button" name="close" onClick="window.close();" data-dismiss="modal" class="btn btn-sm btn-danger">Close!</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div id="app_approve" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" data-dismiss="modal" aria-hidden="true"  class="close">&times;</button>
+                                                    <h4 class="modal-title">Approve Bonus Application</h4></div>
+                                                <div class="modal-body">
+                                                    <div class="form-group">
+                                                        <label class="control-label col-sm-3">Allocate Bonus:</label>
+                                                        <div class="col-sm-9 col-md-4">
+                                                            <input type="hidden" name="app_id" value="<?php echo $app_id; ?>" />
+                                                            <input  class="form-control" placeholder="Amount" type="text" name="allocated_amount" id="3" required />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <input name="process-app" type="submit" class="btn btn-sm btn-success" value="Proceed">
+                                                    <button type="button" name="close" onClick="window.close();" data-dismiss="modal" class="btn btn-sm btn-danger">Close!</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+
+                                <?php if($app_details['enrolment_status'] == '1'){$_meta = $bonus_operations->get_app_meta_by_id($app_id); ?>
+                                    <div class="alert alert-danger ">
+                                        This bonus application has been declined by <?php echo $admin_object->get_admin_name_by_code($_meta['admin_code']); ?> on <?php echo datetime_to_text($_meta['created']) ?>.<br/>
+                                        <strong>Reasons: </strong><?php echo $_meta['comments']?>
+                                    </div>
+                                <?php } ?>
+                                <?php if($app_details['enrolment_status'] == '2'){$_meta = $bonus_operations->get_app_meta_by_id($app_id); ?>
+                                    <div class="alert alert-success">
+                                        This bonus application has been approved by <?php echo $admin_object->get_admin_name_by_code($app_details['compliance_officer']); ?> on <?php echo datetime_to_text($app_details['updated']) ?>.<br/>
+                                        <strong>Allocated Amount: </strong> &dollar; <?php echo number_format($app_details['allocated_amount'], 2, ".", ","); ?><br/>
+                                        <strong>Date Of Allocation: </strong> <?php echo datetime_to_text($app_details['allocation_date'])?><br/>
+                                        <strong>Bonus Status: </strong> <?php echo $bonus_operations->bonus_status($app_details['allocation_status']); ?>
+                                    </div>
+                                <?php } ?>
+
+
 
                                 <table class="table table-responsive table-bordered">
                                     <tbody>
@@ -89,37 +178,6 @@ $conditions = $bonus_operations->get_conditions_by_code($app_details['bonus_code
                                     </tr>
                                     </tbody>
                                 </table>
-
-                                <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="">
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-3">Allocate Bonus:</label>
-                                        <div class="col-sm-9 col-md-4">
-                                            <input class="form-control" placeholder="Amount" type="text" name="allocated_amount" id="3" required />
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="col-sm-offset-3 col-sm-9">
-                                            <button type="button" data-target="#confirm-add" data-toggle="modal" class="btn btn-sm btn-success">Process</button>
-                                        </div>
-                                    </div>
-                                    <!--Modal - confirmation boxes-->
-                                    <div id="confirm-add" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" data-dismiss="modal" aria-hidden="true"  class="close">&times;</button>
-                                                    <h4 class="modal-title">Process Application</h4></div>
-                                                <div class="modal-body">
-                                                    <center>Are you sure you want to process this application?
-                                                        <br/>This action cannot be reversed.</center></div>
-                                                <div class="modal-footer">
-                                                    <input name="process" type="submit" class="btn btn-sm btn-success" value="Proceed">
-                                                    <button type="button" name="close" onClick="window.close();" data-dismiss="modal" class="btn btn-sm btn-danger">Close!</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
                             </div>
                         </div>
 
