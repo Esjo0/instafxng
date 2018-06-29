9b599862faf6f834a5123526a065852b2b70c5b8
Emmanuel Audu <Scargger2010560@gmail.com>
2018-06-29 13:26:35 +0100

    no message

--- public_html/admin/bonus_account_view.php
+++ public_html/admin/bonus_account_view.php
@@ -67,8 +67,8 @@ $_meta = $bonus_operations->get_app_meta_by_id($app_id);
                                         <?php } ?>
                                     </div>
                                 </div>
-
                                 <form data-toggle="validator" class="form-horizontal" role="form" method="post" action="">
+
                                     <div id="app_approve" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
@@ -85,79 +85,74 @@ $_meta = $bonus_operations->get_app_meta_by_id($app_id);
                                         </div>
                                     </div>
 
+                                    <table class="table table-responsive table-bordered">
+                                        <tbody>
+                                            <tr>
+                                                <td><b>Name:</b>  <?php echo $app_details['first_name']; ?> <?php echo $app_details['middle_name']; ?> <?php echo $app_details['last_name']; ?></td>
+                                                <td><b>Application Date:</b>  <?php echo datetime_to_text($app_details['created']); ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td><b>Email:</b>  <?php echo $app_details['email']; ?></td>
+                                                <td><b>Phone:</b>  <?php echo $app_details['phone']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td><b>Account No:</b>  <?php echo $app_details['ifx_acct_no']; ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
 
-                                <table class="table table-responsive table-bordered">
-                                    <tbody>
+                                    <table class="table table-responsive table-bordered">
+                                        <tbody>
                                         <tr>
-                                            <td><b>Name:</b>  <?php echo $app_details['first_name']; ?> <?php echo $app_details['middle_name']; ?> <?php echo $app_details['last_name']; ?></td>
-                                            <td><b>Application Date:</b>  <?php echo datetime_to_text($app_details['created']); ?></td>
+                                            <td><b>Compliance Officer:</b> <?php echo $admin_object->get_admin_name_by_code($app_details['compliance_officer']); ?></td>
+                                            <td><b>Date:</b>  <?php echo datetime_to_text($app_details['updated']) ?></td>
                                         </tr>
                                         <tr>
-                                            <td><b>Email:</b>  <?php echo $app_details['email']; ?></td>
-                                            <td><b>Phone:</b>  <?php echo $app_details['phone']; ?></td>
+                                            <td><b>Allocated Amount:</b>  &dollar; <?php echo number_format($app_details['allocated_amount'], 2, ".", ","); ?></td>
+                                            <td><b>Date Of Allocation:</b>  <?php echo datetime_to_text($app_details['allocation_date'])?></td>
                                         </tr>
                                         <tr>
-                                            <td><b>Account No:</b>  <?php echo $app_details['ifx_acct_no']; ?></td>
+                                            <td><b>Bonus Status:</b>  <?php echo $bonus_operations->bonus_status($app_details['allocation_status']); ?></td>
                                         </tr>
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
 
-                                <table class="table table-responsive table-bordered">
-                                    <tbody>
-                                    <tr>
-                                        <td><b>Compliance Officer:</b> <?php echo $admin_object->get_admin_name_by_code($app_details['compliance_officer']); ?></td>
-                                        <td><b>Date:</b>  <?php echo datetime_to_text($app_details['updated']) ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td><b>Allocated Amount:</b>  &dollar; <?php echo number_format($app_details['allocated_amount'], 2, ".", ","); ?></td>
-                                        <td><b>Date Of Allocation:</b>  <?php echo datetime_to_text($app_details['allocation_date'])?></td>
-                                    </tr>
-                                    <tr>
-                                        <td><b>Bonus Status:</b>  <?php echo $bonus_operations->bonus_status($app_details['allocation_status']); ?></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                                <table class="table table-responsive table-bordered">
-                                    <tbody>
-                                    <tr>
-                                        <td><b>BONUS PACKAGE:</b>  <?php echo $app_details['bonus_title']; ?></td>
-                                    </tr>
-                                    <tr><td><b>PACKAGE DETAILS:</b>  <?php echo $app_details['bonus_desc']; ?></td></tr>
-                                    <tr>
-                                        <td>
-                                            <center><b>PACKAGE CONDITIONS</b></center>
-                                            <table class="table table_responsive">
-                                                <tbody>
-                                                <?php if(!empty($conditions)){ ?>
-                                                    <?php foreach ($conditions as $key => $value){ ?>
-                                                        <?php if(!empty($value)){ ?>
-                                                    <tr>
-                                                        <?php $func = $bonus_conditions->{$value['api']}($app_details[$value['args']]); ?>
-                                                        <td><?php echo $key; ?></td>
-                                                        <td><?php echo $value['title'].'<br/>'.$value['desc']; ?></td>
-                                                        <td class="nowrap"><input class="checkbox" type="checkbox" <?php if($func['status']){ echo 'checked';} ?> value="<?php echo $func['status']?>"  name="condition[<?php echo $key; ?>]"></td>
-                                                    </tr>
+                                    <table class="table table-responsive table-bordered">
+                                        <tbody>
+                                        <tr>
+                                            <td><b>BONUS PACKAGE:</b>  <?php echo $app_details['bonus_title']; ?></td>
+                                        </tr>
+                                        <tr><td><b>PACKAGE DETAILS:</b>  <?php echo $app_details['bonus_desc']; ?></td></tr>
+                                        <tr>
+                                            <td>
+                                                <center><b>PACKAGE CONDITIONS</b></center>
+                                                <table class="table table_responsive">
+                                                    <tbody>
+                                                    <?php if(!empty($conditions)){ ?>
+                                                        <?php foreach ($conditions as $key => $value){ ?>
+                                                            <?php if(!empty($value)){ ?>
+                                                        <tr>
+                                                            <?php $func = $bonus_conditions->{$value['api']}($app_details[$value['args']]); ?>
+                                                            <td><?php echo $key; ?></td>
+                                                            <td><?php echo $value['title'].'<br/>'.$value['desc']; ?></td>
+                                                            <td class="nowrap"><input class="checkbox" type="checkbox" <?php if($func['status']){ echo 'checked';} ?> value="<?php echo $func['status']?>"  name="condition[<?php echo $key; ?>]"></td>
+                                                        </tr>
+                                                            <?php } ?>
                                                         <?php } ?>
-                                                    <?php } ?>
-                                                <?php }else{echo "<tr><td>This bonus package does not have any condition.</td></tr>";} ?>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                                                    <?php }else{echo "<tr><td>This bonus package does not have any condition.</td></tr>";} ?>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
                                 </form>
                             </div>
                         </div>
-
                     </div>
-
                     <!-- Unique Page Content Ends Here
                     ================================================== -->
-                    
                 </div>
-                
             </div>
         </div>
         <?php require_once 'layouts/footer.php'; ?>
