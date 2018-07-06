2c6392faa0ff187a39439e65b337b9ea0957343b
Emmanuel Audu <Scargger2010560@gmail.com>
2018-07-05 23:34:40 -0700

    no message

--- cronjobs/signal_management/trigger_monitor.php
+++ cronjobs/signal_management/trigger_monitor.php
@@ -2,3 +2,5 @@
 set_include_path('../../public_html/init/');
 require_once 'initialize_general.php';
 $signal_object = new Signal_Management();
+$date_time = date('Y-m-d h:m');
+$query = "SELECT signal_id, price, CONCAT(trigger) FROM signal_daily ";
