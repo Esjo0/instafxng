9efe0ba1a3c6b999719c54f9af6455c7ef859e8f
Emmanuel Audu <Scargger2010560@gmail.com>
2018-08-01 15:58:34 +0100

    Updates

--- app_assets/class_bonus_conditions.php
+++ app_assets/class_bonus_conditions.php
@@ -2,7 +2,7 @@
 class Bonus_Condition{
     public $BONUS_CONDITIONS = array(
         1 => array(
-            'title' => 'New Account Validation',
+            'title' => 'New Account',
             'desc' => 'All participating Instaforex accounts must be new accounts.',
             'type' => 0,
             'extra' => '',
@@ -10,87 +10,6 @@ class Bonus_Condition{
             'args' => array('bonus_account_id'),
             'returns' => array('status')
         ),
-        2 => array(
-            'title' => 'ILPR Account Validation',
-            'desc' => 'All participating Instaforex accounts must be enrolled into the Instafxng Loyalty Promotion and Rewards (ILPR).',
-            'type' => 0,
-            'api' => 'valid_ilpr_account_cond',
-            'extra' => '',
-            'args' => array('bonus_account_id'),
-            'returns' => array('status')
-        ),
-        3 => array(
-            'title' => 'Client Verification',
-            'desc' => 'All clients benefiting from this bonus package must have verified their identities.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => '',
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        4 => array(
-            'title' => 'Bonus Expiry (Case 1)',
-            'desc' => 'Bonuses would be withdrawn from any Instaforex account that benefits from this bonus package and fails to trade within the specified window period.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Window'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        5 => array(
-            'title' => 'Bonus Withdrawal (Case 1)',
-            'desc' => 'Bonuses would be withdrawn from an Instaforex account benefiting from this bonus package trades less than the specified volume within the period of the bonus allocation.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Volume'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        6 => array(
-            'title' => 'Bonus Withdrawal (Case 2)',
-            'desc' => 'Each withdrawal order from any benefiting Instaforex must not be more than the recommended amount or the stated percentage of the allocated bonus.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Percentage'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        7 => array(
-            'title' => 'Profit Withdrawal (Case 1)',
-            'desc' => 'Allow or disallow profit withdrawal from benefiting Instaforex account numbers on this bonus package.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Status'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        8 => array(
-            'title' => 'Profit Withdrawal (Case 1)',
-            'desc' => 'Allow or disallow profit withdrawal from benefiting Instaforex account numbers on this bonus package.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Status'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        9 => array(
-            'title' => 'Profit Withdrawal (Case 1)',
-            'desc' => 'Allow or disallow profit withdrawal from benefiting Instaforex account numbers on this bonus package.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Status'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        ),
-        10 => array(
-            'title' => 'Profit Withdrawal (Case 1)',
-            'desc' => 'Allow or disallow profit withdrawal from benefiting Instaforex account numbers on this bonus package.',
-            'type' => 0,
-            'api' => 'bonus_withdrawal_case_1_cond',
-            'extra' => array('Status'),
-            'args' => array('bonus_account_id'),
-            'returns' => array('status', 'docs')
-        )
     );
 
     public $CONDITIONS_TYPES = array(
@@ -108,6 +27,11 @@ class Bonus_Condition{
     }
 
     public function new_account_cond($bonus_account_id){
+        global $db_handle;
+        $query = "SELECT ifx_account_id, 
+        FROM bonus_accounts AS BA 
+        INNER JOIN user_deposit AS UD ON UD.
+        INN";
         $result['status'] = true;
         return $result;
     }
