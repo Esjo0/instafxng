5caa54804b6cf2ae5c5895be810d0176ec9a89e4
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-08-02 09:34:42 +0100

    no message

--- public_html/signal_schedules.php
+++ public_html/signal_schedules.php
@@ -219,12 +219,12 @@ $scheduled_signals = $signal_object->get_scheduled_signals(date('Y-m-d'));
 <!--        };-->
 <!--        firebase.initializeApp(config);-->
 <!--    </script>-->
-<!--    <script>-->
-<!--        signal.new_signal_listener();-->
-<!--        signal.getQuotes();-->
-<!--        setInterval(function(){signal.new_signal_listener();}, 120000);//TODO: Fix this back to 5000-->
-<!--        setInterval(function(){signal.getQuotes();}, 60000);//TODO: Fix this back to 5000-->
-<!--    </script>-->
+    <script>
+        signal.new_signal_listener();
+        //signal.getQuotes();
+        setInterval(function(){signal.new_signal_listener();}, 120000);//TODO: Fix this back to 5000
+        //setInterval(function(){signal.getQuotes();}, 60000);//TODO: Fix this back to 5000
+    </script>
 <!--<script>-->
 <!--    function cal_gain(id) {-->
 <!--        var equity = parseInt(document.getElementById('signal_equity_'+id).value);-->
