fd90c7a9fda0c04b887e021112923208807d4a34
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-08-25 10:18:46 +0100

    no message

--- public_html/layouts/notify.php
+++ public_html/layouts/notify.php
@@ -9,10 +9,11 @@ $query = "SELECT content FROM advert_div WHERE status = 1 LIMIT 1";
 $result = $db_handle->runQuery($query);
 $notification = $db_handle->fetchAssoc($result);
 ?>
+<?php if(!empty($notification)){ ?>
     <div id="advert" style="max-width:500px;  background-color: #ff0214; color: white; display: none; position: fixed; bottom: 10px; right: 10px;" class="alert pull-right fade in">
         <?php  foreach($notification AS $row){extract($row)?>
                 <?php echo $content;?>
         <?php }?>
     </div>
-
+<?php }?>
 
