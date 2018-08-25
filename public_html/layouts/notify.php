60099da30e31d058e1caee719e4d0f8601edd88a
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-08-25 10:34:24 +0100

    no message

--- public_html/layouts/notify.php
+++ public_html/layouts/notify.php
@@ -11,6 +11,7 @@ $notification = $db_handle->fetchAssoc($result);
 ?>
 <?php if(!empty($notification)){ ?>
     <div id="advert" style="max-width:500px;  background-color: #ff0214; color: white; display: none; position: fixed; bottom: 10px; right: 10px;" class="alert pull-right fade in">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         <?php  foreach($notification AS $row){extract($row)?>
                 <?php echo $content;?>
         <?php }?>
