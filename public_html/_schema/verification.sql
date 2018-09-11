fb1a2f713fe4fa91af414aadbfa7a81162e73790
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-09-11 15:59:20 +0100

    no message

--- public_html/_schema/verification.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `user_credential` CHANGE `status` `status` ENUM('1','2','3') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '1' COMMENT '1 - Awaiting Moderation2 - Moderated3 - Pending';
-
-CREATE TABLE `user_credential_remark` (
- `credential_id` varchar(100) NOT NULL,
- `remark` text NOT NULL,
- `admin` varchar(100) NOT NULL,
- `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
