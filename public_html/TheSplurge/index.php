3ddd3bd57f1c26d1863170790d70fb5e4928e246
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-10-17 06:16:41 +0100

    no message

---
+++ public_html/TheSplurge/index.php
@@ -0,0 +1,51 @@
+<?php
+require_once '../init/initialize_general.php';
+$thisPage = "Home";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <base target="_self">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Instaforex Nigeria | Online Instant Forex Trading Services</title>
+        <meta name="title" content="Instaforex Nigeria | Online Instant Forex Trading Services" />
+        <meta name="keywords" content="instaforex, forex trading in nigeria, forex seminar, forex trading seminar, how to trade forex, trade forex, instaforex nigeria">
+        <meta name="description" content="Instaforex, a multiple award winning international forex broker is the leading Forex broker in Nigeria, open account and enjoy forex trading with us.">
+        <?php require_once '../layouts/head_meta.php'; ?>
+        
+        <meta property="og:site_name" content="Instaforex Nigeria" />
+        <meta property="og:title" content="Instaforex Nigeria | Online Instant Forex Trading Services" />
+        <meta property="og:description" content="Instaforex, a multiple award winning international forex broker is the leading Forex broker in Nigeria, open account and enjoy forex trading with us." />
+        <meta property="og:image" content="images/instaforex-100bonus.jpg" />
+        <meta property="og:url" content="https://instafxng.com/" />
+        <meta property="og:type" content="website" />
+        <script src="https://www.wpiece.com/js/webcomponents.min.js"></script>
+        <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.png">
+        <link  rel="import" href="http://www.wpiece.com/p/10_26" />
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Bootstrap core CSS -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.12/css/mdb.min.css" rel="stylesheet">
+
+        <!-- JQuery -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.12/js/mdb.min.js"></script>
+
+    </head>
+    <body>
+        <!-- Main Body: The is the main content area of the web site, contains a side bar  -->
+        <div id="main-body" class="container-fluid" >
+            <img id="img_div" class="img img-responsive img-thumbnail" src="https://instafxng.com/imgsource/splurge.png" style="width:100vw; height:100vh; margin-top:5px; box-shadow: 0 4px 8px 0 rgba(255, 11, 0, 0.75), 0 6px 20px 0 rgba(255, 11, 0, 0.83)">
+        </div>
+    </body>
+</html>
