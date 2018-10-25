5fe82d8d0ade62763acbb9558f188c9df7547dc0
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-10-25 07:23:57 +0100

    completion certificate

---
+++ public_html/fxacademy/certificate.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 25/10/2018
+ * Time: 7:06 AM
+ */
+require_once '../init/initialize_client.php';
+$thisPage = "";
+
+if (!$session_client->is_logged_in()) {
+    redirect_to("login.php");
+}
+$get_learning_position = $education_object->get_learning_position($_SESSION['client_unique_code']);
+$highest_lesson_published = $education_object->get_highest_lesson_published();
+
+if($get_learning_position != $highest_lesson_published){
+    redirect_to("./");
+}
+$full_name = $_SESSION['client_last_name'] . ' ' . $_SESSION['client_first_name'];
+$message_final = <<<MAIL
+   <body style="background-image: url(../images/cert.jpg);
+                background-repeat: no-repeat;
+                background-size: cover;
+                height: 1200px;
+                width: 650px;">
+    <div style="font-family: 'Times New Roman'; font-size: 30px; margin-left: 450px; padding-top:380px;" id="name">$full_name</div>
+    </body>
+MAIL;
+
+$mpdf = new \Mpdf\Mpdf([
+    'orientation' => 'L'
+]);
+$mpdf->SetDisplayMode('fullpage');
+$mpdf->WriteHTML($message_final);
+$mpdf->Output($full_name .'.pdf', \Mpdf\Output\Destination::DOWNLOAD);
--- public_html/fxacademy/completion_cert.php
+++ public_html/fxacademy/completion_cert.php
@@ -25,16 +25,6 @@ if($get_learning_position != $highest_lesson_published){
         <meta name="keywords" content="">
         <meta name="description" content="">
         <?php require_once 'layouts/head_meta.php'; ?>
-        <script>
-            function print_report(divName) {
-                var printContents = document.getElementById(divName).innerHTML;
-                var originalContents = document.body.innerHTML;
-                document.body.innerHTML = printContents;
-                window.print();
-                document.body.innerHTML = originalContents;
-            }
-        </script>
-
     </head>
     <body>
         <?php require_once 'layouts/header.php'; ?>
@@ -59,37 +49,15 @@ if($get_learning_position != $highest_lesson_published){
                                 <p>You can now comfortably take live trades in the Forex Market and make as much profit as you desire.
                                     At this point, you qualify for our 100% education bonus* for funding your account.</p>
                                 <p><a class="btn btn-success btn-lg" target="_blank" href="https://instafxng.com/deposit.php">Fund Account - Get Bonus Now</a></p>
-                                <p style="display:none;"><button onclick="print_report('print_out');" type="button" class="btn btn-lg btn-success">Download Your Certificate</button></p>
+                                <p ><a class="btn btn-success btn-lg" href="https://instafxng.com/fxacademy/certificate.php">Download Your Certificate</a></p>
                                 <p><a class="btn btn-success btn-lg" target="_blank" href="https://instafxng.com/fxacademy/schedule_training.php">Schedule A free one-on-one training with our analyst</a></p>
                                 <hr />
                             </div>
                         </div>
                     </div>
-                    <div style="display: none;" id="print_out">
-                        <div class="row">
-                            <div class="col-md-2"></div>
-                        <div class="col-md-8" style="height:100%; width:100%; padding:20px; text-align:center; border: 5px solid #787878">
-                            <img src="images/ifxlogo.png" alt="Instaforex Nigeria Logo" /></br>
-                            <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
-                            <br><br>
-                            <span style="font-size:25px"><i>This is to certify that</i></span>
-                            <br><br>
-                            <span style="font-size:30px"><b><?php echo $_SESSION['client_last_name'] . ' ' . $_SESSION['client_first_name']; ?></b></span><br/><br/>
-                            <span style="font-size:25px"><i>
-                                    has satisfactorily completed the requirements for completion of the course and is hereby awarded this Certificate Of Completion. In withness, we acknowledge your learing and studies at the Forex Academy.
-                                    </i></span> <br/>
-                            <span style="font-size:30px">----------</span> <br/><br/>
-                            <img class="pull-left" src="https://instafxng.com/imgsource/demolas_signature.png" width="100" height="65"><br/><br/><br/><br>
-                            <span style="font-size:20px; text-align:left;" class="pull-left">Ademola Oyebode</span> <br/><br/>
-                            <span style="font-size:15px; text-align:left;" class="pull-left">Director Of Programs.</span><br/><br/>
-                            <span style="font-size:10px"><i>www.instafxng.com/fxacademy</i></span>
-                        </div>
-                            <div class="col-md-2"></div>
-                            </div>
-                    </div>
-                    </div>
+                </div>
         </div>
-
+        </div>
         <?php require_once 'layouts/footer.php'; ?>
     </body>
 </html>
