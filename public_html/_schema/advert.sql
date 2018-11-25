84cd7612cd2ed886d944b7677247f832883d5a93
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-11-25 16:45:18 +0100

    Dinner admin end to manage all dinner tickets

--- public_html/_schema/advert.sql
+++ public_html/_schema/advert.sql
@@ -1,3 +1,7 @@
+ALTER TABLE `dinner_2018` CHANGE `type` `type` ENUM('1','2','3','4','5','6') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '1-Single 2-Double 3-VIP 4-VVIP 5-Team 6-Vendor';
+ALTER TABLE `dinner_2018` ADD UNIQUE(`email`);
+ALTER TABLE `dinner_2018` CHANGE `user_code` `user_code` VARCHAR(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
+
 ALTER TABLE `black_friday_2018` CHANGE `tire` `tire` ENUM('0','1','2','3','4','5') CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '1- Platinum 2 - Gold 3- Silver1 4- Silver2 5 - Bronzr1 6- Bronze2';
 
 CREATE TABLE IF NOT EXISTS `onboarding_analytics` (
