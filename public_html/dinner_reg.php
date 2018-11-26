eb23adff9d62c6e5d4eb7286e5e98753e79b1bfb
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-11-26 17:05:17 +0100

    no message

--- public_html/dinner_reg.php
+++ public_html/dinner_reg.php
@@ -6,7 +6,6 @@ $dinner_emails = array("joshua@instafxng.com", "binaryvs@yahoo.com", "amazuci@ya
 if(isset($_POST['submit3'])){
     $email = $db_handle->sanitizePost($_POST['email']);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-var_dump($email);
     if(in_array($email, $dinner_emails) ) {
 $query  = "SELECT user_code FROM user WHERE email = '$email'";
         $result = $db_handle->runQuery($query);
