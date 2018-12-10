071c275bbba4a6e6dfc281cca6000d0d35b26bc0
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2018-12-10 17:17:31 +0100

    no message

--- public_html/dinner_invite_code_script.php
+++ public_html/dinner_invite_code_script.php
@@ -3,7 +3,7 @@ require_once 'init/initialize_general.php';
 
 $client_operation = new clientOperation();
 
-$query = "SELECT * FROM dinner_2018";
+$query = "SELECT * FROM dinner_2018 WHERE invite_code IS NULL AND choice = '1'";
 $result = $db_handle->runQuery($query);
 $result = $db_handle->fetchArray($result);
 foreach($result AS $row){
@@ -17,7 +17,7 @@ else if($type == 5){$ticket = 'TEAM';}
 else if($type == 6){$ticket = 'VENDOR';}
     $invite_code = $id.$title[0].$gender.$town[0].$state[0]."~".$ticket;
     $query = "UPDATE dinner_2018 SET invite_code = '$invite_code'
-                          WHERE email = '$email'";
+                          WHERE email = '$email' AND invite_code IS NULL";
     $result = $db_handle->runQuery($query);
 }
 }?>
