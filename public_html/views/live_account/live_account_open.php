f0765938e4a95c4269e73dd40ea4cee4957ee732
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2019-01-09 15:52:21 +0100

    no message

--- public_html/views/live_account/live_account_open.php
+++ public_html/views/live_account/live_account_open.php
@@ -1,12 +1,17 @@
 <ul class="fa-ul">
     <li><i class="fa-li fa fa-check-square-o icon-tune"></i>Step 1: Open Live Account</li>
 </ul>
-<p class="text-success"><strong>The Account Opening Form is Loading... Kindly wait for it, fill the form and then proceed to Step 2.</strong></p>
-
+<p class="text-success text-center"><strong>The Account Opening Form is Loading... Kindly wait for it, fill the form and then proceed to Step 2.</strong>
+<br>
+    <span class="text-danger"><i class="fa fa-info-circle"></i> Ensure to fill in your <b>Full Name</b> as it is on your valid means of identification(ID card).</span>
+</p>
+<div class="text-center">
 <iframe class="embed-responsive-item" id="frame" style='width: 780px;border:0;height:auto;' src="https://secure.instaforex.com/en/partner_open_account.aspx?x=BBLR&width=580&showlogo=false&color=red&host="https://instafxng.com/live_account.php" style="padding:0; margin:0" scrolling="no" onload="var th=this; setTimeout(function() {var h=null;if (!h) if (location.hash.match(/^#h(\d+)/)) h=RegExp.$1;if (h) th.style .height=parseInt(h)+170+'px';}, 10);" ></iframe>
 <script> document.getElementById('frame').src += window.location; </script>
-<form class="form-horizontal" role="form" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+
+<form class="form-horizontal row" role="form" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
     <div class="form-group">
-        <div class="col-sm-offset-3 col-sm-9"><input name="live_account_open" type="submit" class="btn btn-success" value="Proceed to Step 2" title="Click here to proceed to Step 2" /></div>
+        <div class="col-sm-12"><input name="live_account_open" type="submit" class="btn btn-success" value="Proceed to Step 2" title="Click here to proceed to Step 2" /></div>
     </div>
 </form>
+</div>
