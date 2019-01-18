72490ac4dae19b1b1ebd740920c17a15968e37ff
LAPTOP-JDERRBJH\user <joshuaesan1@gmail.com>
2019-01-18 17:02:43 +0100

    no message

--- app_assets/class_access_controller.php
+++ app_assets/class_access_controller.php
@@ -186,9 +186,10 @@ define("PAGE_CODE", json_encode(array(
     303 => "unified_withdrawals.php",
     304 => "withdrawal_completed_unified.php",
     305 => "onboarding_campaign.php",
-    306 => "onboarding_campaign_review.php"
+    306 => "onboarding_campaign_review.php",
+    307 => "clients_review.php"
 
-    )));
+)));
 class Access_Controller
 {
     public function get_all_pages(){
